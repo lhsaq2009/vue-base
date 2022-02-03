3eca5b44d8af3515404a6180e8250cb623b85d46
haisen <lhsaq2009@163.com>
2022-02-03 20:01:38 +0800

    解决：页面缓存， "lru-cache": "^4.1.5"

--- server.js
+++ server.js
@@ -32,41 +32,44 @@ function renderToString(context) {
 
 // TODO：haisen 没生效，2022-02-02
 const LRU = require('lru-cache');               // 删除最近最少使用条目的缓存对象
-// 实例化配置缓存对象
-const microCache = new LRU({
-    max: 1000,                                  // 最大存储 100 条
-    maxAge: 1000                                // 存储在 1 秒后过期
+
+// "lru-cache": "^4.1.5",
+const microCache = LRU({
+    max: 100,
+    maxAge: 1000 * 60 * 60
 });
 
 // 路由处理交给 vue
 app.get('*', async (req, res) => {
     try {
-        // 根据 url 获取缓存页面
-        // const hit = microCache.get(req.url);
-        // // 如果有缓存则直接返回缓存数据
-        // if (hit) {
-        //     console.log("hit return");
-        //     return res.end(hit);
-        // }
+        const cacheable = isCacheable(req);
+        if (cacheable) {
+            const hit = microCache.get(req.url)
+            if (hit) {
+                console.log("hit：" + req.url);
+                return res.end(hit)
+            }
+        }
 
         // window ? window.ssr_cookie = req.cookie : undefined;
 
-        console.log("res：----------------");
-        // console.log(res);
         console.log(req.url);
 
         const context = {
             url: req.url,
             title: 'index.template.html test from server.js'
         };
-        
+
         // console.log("process.env.RUN_ENV：" + process.env.RUN_ENV);
         const html = await renderToString(context);
 
         res.setHeader('Content-Type', 'text/html; charset=utf-8');
         res.send(html);
+
         // 将页面缓存到缓存对象中
-        // microCache.set(req.url, html);
+        if (cacheable) {
+            microCache.set(req.url, html)
+        }
     } catch (e) {
         res.send('服务端异常：' + e);
     }
@@ -76,3 +79,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('App running at: 3000');
 });
+
+const isCacheable = req => {
+    return req.url
+}
